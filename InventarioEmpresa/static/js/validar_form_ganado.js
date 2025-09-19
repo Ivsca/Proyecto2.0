3aef5281322078149cdc694bb8cd5bedc9b46bd6
Yojan <117781973+yojan-alverto@users.noreply.github.com>
2025-09-19 07:52:10 -0500

    Eliminando carpetas con las versiones anteriores del proyecto

--- InventarioEmpresa/static/js/validar_form_ganado.js
+++
@@ -1,9 +0,0 @@
-function validar(input) {
-    if (input.value !== '') {
-        input.classList.add('valid');
-        input.classList.remove('invalid');
-    } else {
-        input.classList.add('invalid');
-        input.classList.remove('valid');
-    }
-}
--- Version 1 Proyecto/README.md
+++
@@ -1,2 +0,0 @@
-# Yojan se encargó de diseñar los inventarios para el registro de la información del ganado o vegetales
-# Espacio para cargar los archivos rar con el html y css de la pagina en general del proyecto
