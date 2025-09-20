fbcc3666866a8897366ed09bc61324606f33e31f
Yojan <117781973+yojan-alverto@users.noreply.github.com>
2025-09-20 08:29:18 -0500

    elimine todas las versiones anteriores que no estamos usando

--- inventario_cysla/inventario_cysla/decoradores.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import redirect
-
-def login_requerido(view_func):
-    def wrapper(request, *args, **kwargs):
-        if not request.session.get("usuario_id"):
-            return redirect("LoginUser")
-        return view_func(request, *args, **kwargs)
-    return wrapper
