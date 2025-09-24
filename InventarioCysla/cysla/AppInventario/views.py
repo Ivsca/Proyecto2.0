a68634c00e6c9210190ccf7fbe0a3515d998d215
Yojan <117781973+yojan-alverto@users.noreply.github.com>
2025-09-24 10:21:19 -0500

    ganado inactivo intento 13

--- InventarioCysla/cysla/AppInventario/views.py
+++ InventarioCysla/cysla/AppInventario/views.py
@@ -27,6 +27,7 @@ from django.core.serializers import serialize
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 
+from django.contrib.auth.decorators import login_required
 
 from django.contrib import messages
 
