11f6fd3e296c93fceef65e2a3fc5891c7e7db931
Yojan <117781973+yojan-alverto@users.noreply.github.com>
2025-09-24 10:31:32 -0500

    ganado inactivo intento 15

--- InventarioCysla/cysla/AppInventario/decoradores.py
+++ InventarioCysla/cysla/AppInventario/decoradores.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect
 
-def login_requerido(view_func):
+def login_required(view_func):
     def wrapper(request, *args, **kwargs):
         if not request.session.get("usuario_id"):
             return redirect("LoginUser")
